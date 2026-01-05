8020f3d51079b530df1adeb3485b944204aca758
alex <alexchernushenko@gmail.com>
2026-01-05 20:22:15 +0200

    Another one day going by

---
+++ src/SQL/LC_570.sql
@@ -0,0 +1,21 @@
+/*Table: Employee
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| department  | varchar |
+| managerId   | int     |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table indicates the name of an employee, their department, and the id of their manager.
+If managerId is null, then the employee does not have a manager.
+No employee will be the manager of themself.
+Write a solution to find managers with at least five direct reports.*/
+
+SELECT a.name 
+FROM Employee a 
+JOIN Employee b 
+ON a.id = b.managerId 
+GROUP BY b.managerId 
+HAVING COUNT(*) >= 5
---
+++ src/SQL/LC_577.sql
@@ -0,0 +1,32 @@
+/*Table: Employee
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| empId       | int     |
+| name        | varchar |
+| supervisor  | int     |
+| salary      | int     |
++-------------+---------+
+empId is the column with unique values for this table.
+Each row of this table indicates the name and the ID of an employee in addition to their salary and the id of their manager.
+
+Table: Bonus
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| empId       | int  |
+| bonus       | int  |
++-------------+------+
+empId is the column of unique values for this table.
+empId is a foreign key (reference column) to empId from the Employee table.
+Each row of this table contains the id of an employee and their respective bonus.
+Write a solution to report the name and bonus amount of each employee who satisfies either of the following:
+    The employee has a bonus less than 1000.
+    The employee did not get any bonus.
+*/
+
+SELECT e.name, b.bonus 
+FROM Employee e
+LEFT JOIN Bonus b
+ON e.empId = b.empId
+WHERE b.bonus IS NULL OR b.bonus < 1000; 
